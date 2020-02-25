fbf5ab7e2e880ff3966fb58cdb683aed08e531d3
Gao Yuan <ksws0334789@outlook.com>
2020-02-25 14:53:47 +0800

    step1 写了step1的测试代码，并成功运行

---
+++ src/test/java/HelloWorldService.java
@@ -0,0 +1,10 @@
+/**
+ * @author Gao Yuan
+ * @date 2020-02-25 - 14:48
+ */
+public class HelloWorldService {
+
+    public void helloWorld() {
+        System.out.println("hello, world!");
+    }
+}
